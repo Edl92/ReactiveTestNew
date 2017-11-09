dd9e296044008b1d8de73e3029863f488fa5ed52
Eugenio di Lorenzo <gege92@teletu.it>
2017-11-09 17:32:06 +0100

    Create Proj

---
+++ ReactiveTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReactiveTest
+//
+//  Created by Wind3 on 09/11/2017.
+//  Copyright © 2017 Wind3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
