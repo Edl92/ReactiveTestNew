27be8e3bf24255278c313b7561545df9c37d8cc8
Eugenio di Lorenzo <gege92@teletu.it>
2017-11-10 12:31:31 +0100

    Aggiunti esempi Reactive

--- ReactiveTest/ViewController.swift
+++ ReactiveTest/ViewController.swift
@@ -7,13 +7,90 @@
 //
 
 import UIKit
+import ReactiveCocoa
 import ReactiveSwift
-
+import Result
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label:UILabel?
+    @IBOutlet weak var button:UIButton?
+    var sg : SignalProducer<String, NoError>?
+    var clock : Signal<Date, NoError>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        var i = 0
+        
+        sg = SignalProducer<String,NoError>{(sink, disposable) -> () in
+           sink.send(value: "Hello World")
+           sink.sendCompleted()
+        
+        }.map({ (s: String) -> String in
+            return s.uppercased()
+        })
+        
+       /* clock = Signal<Date,NoError>{ (sink)->Disposable? in
+            
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (t) in
+             //   print("tick\(i)")
+                i += 1
+                sink.send(value: Date())
+            })
+        
+            return AnyDisposable()
+        }
+        
+        clock?.observeValues { (d: Date) in
+            print("Received tick at \(d)")
+        }
+ 
+        
+        
+        var disposable: Disposable?
+        disposable = clock?.observeValues { (d: Date) in
+            print("Received value \(d)")
+            
+            if i == 10 {
+                disposable!.dispose()
+            }
+        }
+        
+        let sp = SignalProducer<Date, NoError> { sink, disposable in
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer: Timer) in
+                print("tick")
+                sink.send(value: Date())
+            })
+        }
+        */
+
+        
+        
+        let sp = SignalProducer<Date, NoError> { sink, disposable in
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer: Timer) in
+                // check if the disposable has been disposed
+                if disposable.hasEnded {
+                    timer.invalidate()
+                }
+                print("tick")
+                sink.send(value: Date())
+            })
+        }
+        
+        sp.startWithValues { (d) in
+            print(d)
+        }
+        
+        var disposable: Disposable?
+        disposable = sp.startWithValues { (time: Date) in
+            print("time: \(time)")
+            i += 1
+            if i == 10 {
+                disposable!.dispose()
+            }
+        }
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -23,6 +100,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func didPressButton(sender:UIButton){
+        
+
+        sg?.startWithValues({ (input : String) in
+            self.label?.text = input
+        })
+        
+    }
     
 
 }
